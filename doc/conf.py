8c5bde24d29a0ac1181b24a2cba6f7b568b61db2
bygu4 <sacha05bugaev@gmail.com>
2025-01-01 22:03:15 +0300

    update sphinx config for using type annotations

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -295,6 +294,11 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
+# Use type hints in both signature and description.
+autodoc_typehints = "both"
+
+# Use type annotations for documenting the return type.
+napoleon_use_rtype = False
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
